37c1da76034c319d64f7b78ed550947249d0764c
Robert Rovenko <robert.ntisthlm@gmail.com>
2024-10-12 00:30:59 +0200

    trying to fix an eternal error when deploying

---
+++ .next/server/pages/JobDetail.js
@@ -0,0 +1,66 @@
+"use strict";(()=>{var e={};e.id=295,e.ids=[295,660],e.modules={5160:(e,i,t)=>{t.r(i),t.d(i,{config:()=>w,default:()=>x,getServerSideProps:()=>y,getStaticPaths:()=>j,getStaticProps:()=>b,reportWebVitals:()=>v,routeModule:()=>C,unstable_getServerProps:()=>P,unstable_getServerSideProps:()=>D,unstable_getStaticParams:()=>S,unstable_getStaticPaths:()=>N,unstable_getStaticProps:()=>k});var a={};t.r(a),t.d(a,{default:()=>f});var s=t(7687),r=t(9057),n=t(3639),o=t(5130),l=t.n(o),c=t(79),d=t.n(c),m=t(997);t(6689);var h=t(4661),p=t(9816);let g=[{id:1,name:"INIU Mini Powerbank",shortDescription:`
+              Compact and powerful, the INIU Mini Power Bank features a 10,000mAh capacity with 22.5W fast charging and USB-C compatibility. 
+              Charges up to 3 devices at once.
+          `,longDescription:`
+              The INIU Mini Power Bank offers a 10,000mAh capacity with 22.5W fast charging and USB-C in & out.
+        
+              **Key Features:**
+              - **Compatibility**: Works with iPhone 15/14/13/12/11, iPad, Samsung S22/S21, Xiaomi, Google devices, and more.
+              - **Performance**: Rated 4.6 out of 5 stars from 15,982 reviews.
+              - **Capacity**: Compact design, half the size of an iPhone, charges up to 3 devices simultaneously.
+              - **Support**: Includes a USB-C cable, travel case, user manual, and comes with a 30-day money-back guarantee, a 5-year warranty, and lifetime technical support.
+        
+              Priced at 242.65 kr with a 16% discount off the median price of 289.99 kr.
+          `,images:["/images/iniuminipowerbank.jpg","/images/iniuminipowerbank.jpg"],affiliateLink:"https://amzn.to/3BfMraU",price:"242,65 kr"},{id:2,name:"Xiaomi Smart Band 8",shortDescription:`
+              Sleek and versatile, the Xiaomi Smart Band 8 features a 1.62” AMOLED display, 16-day battery life, and 200+ customizable watch faces. 
+              Tracks health and supports 150+ workout modes.
+          `,longDescription:`
+              The Xiaomi Smart Band 8 features a sleek design with a metallic-like frame and quick-release strap for easy customization.
+        
+              **Key Features:**
+              - **Display**: Ambient light sensor with 60Hz refresh rate for smooth performance.
+              - **Battery Life**: Up to 16 days with 2C fast charging, fully recharged in about 1 hour.
+              - **Customization**: Choose from over 200 watch faces and track health with continuous SpO2 measurement, heart rate monitoring, and sleep analysis.
+              - **Exercise Tracking**: 150+ workout modes, supports unique accessories for versatile wear.
+        
+              Specifications include a 1.62” AMOLED screen, 5 ATM water resistance, and compatibility with Android 6.0 and iOS 12 or higher. Priced at 391,99 kr.
+          `,images:["/images/xioamismartband8.jpg","/images/xioamismartband8.jpg"],affiliateLink:"https://amzn.to/3XlditG",price:"391,99 kr"},{id:3,name:"Logitech C270 HD",shortDescription:`
+              The Logitech C270 HD Webcam offers 720p widescreen video, automatic light correction, and a noise-canceling microphone for clear calls.
+          `,longDescription:`
+              The Logitech C270 HD Webcam is designed for high-quality video calls with a 720p resolution and automatic light adjustment for better visibility in low-light conditions.
+        
+              **Key Features:**
+              - **Resolution**: HD 720p at 30fps for clear, widescreen video calls.
+              - **Microphone**: Built-in noise-canceling microphone for clear audio in noisy environments.
+              - **Light Adjustment**: Automatic light correction for better image quality in various lighting conditions.
+              - **Design**: Compact and adjustable, fits easily on monitors or stands on a shelf.
+              - **Compatibility**: Works with Skype, FaceTime, Hangouts, WebEx, and is certified for Chromebook.
+        
+              Priced at 190,24 kr with a 10% discount off the median price of 212,49 kr.
+          `,images:["/images/logitechc270.jpg","/images/logitechc270.jpg"],affiliateLink:"https://amzn.to/4e0DQrc",price:"190,24 kr"},{id:4,name:"Hand Grip Trainer",shortDescription:`
+              Enhance hand, finger, wrist, and forearm strength with the Vicloon Hand Grip Trainer. Adjustable resistance from 10-50 kg and non-slip handles for comfortable use.
+          `,longDescription:`
+              The Vicloon Hand Grip Trainer is perfect for improving grip strength and rehabilitating hand injuries.
+        
+              **Key Features:**
+              - **Adjustable Resistance**: Easily adjustable from 10-50 kg (22-110 lbs) using a rotating knob.
+              - **Non-Slip Handles**: Covered with eco-friendly TPE material for a secure grip, even when sweaty.
+              - **Portable**: Lightweight (150 g), compact (15.5 cm long), easy to carry and store.
+              - **Versatile**: Suitable for all ages and fitness levels, ideal for strengthening and rehabilitation.
+              - **Gift Idea**: Perfect for various occasions like birthdays and holidays.
+        
+              Priced at 73,30 kr with a 5% discount off the previous price of 77,16 kr, median price 89,99 kr.
+          `,images:["/images/vicloonhandgrip.jpg","/images/vicloonhandgrip.jpg"],affiliateLink:"https://amzn.to/3MOa0u3",price:"73,30 kr"},{id:5,name:"Philips 3000 Mixer",shortDescription:`
+            The Philips 3000 Series Mixer features a ProBlend system, 1.9 L max capacity, and a 450W motor. Includes a HomeID app with recipe ideas and a pulse function.
+        `,longDescription:`
+            The Philips 3000 Series Mixer offers efficient blending with a 1.9 L max capacity and a 450W motor. Perfect for smoothies and sauces.
+      
+            **Key Features:**
+            - **ProBlend System**: Designed for smooth blending with optimized blade and jar design.
+            - **Capacity**: 1.9 L maximum capacity, with a 1 L effective capacity.
+            - **Motor**: 450W motor for powerful performance and ice crushing.
+            - **Speed Settings**: One speed setting plus a pulse function for versatile use.
+            - **HomeID App**: Includes a free app with a variety of recipe ideas.
+      
+            Priced at 428,30 kr.
+        `,images:["/images/philips3000mixer.jpg","/images/philips3000mixer2.jpg"],affiliateLink:"https://amzn.to/3ziKeep",price:"428,30 kr"}],u=()=>{let e=Math.floor(Math.random()*g.length),i=Math.floor(Math.random()*g.length);for(;i===e;)i=Math.floor(Math.random()*g.length);return[g[e],g[i]]},f=()=>{let{jobId:e}=(0,h.useParams)(),i=(0,h.useNavigate)(),t=p.Z.find(i=>i.id===e);if(!t)return m.jsx("div",{className:"text-center",children:"Job not found."});let[a,s]=u();return(0,m.jsxs)("div",{className:"relative bg-gradient-to-r from-purple-800 to-indigo-600 py-10",children:[" ",m.jsx("div",{className:"absolute inset-0 bg-black opacity-50"})," ",(0,m.jsxs)("div",{className:"container mx-auto flex flex-col md:flex-row justify-between px-4 relative z-10",children:[" ",(0,m.jsxs)("div",{className:"hidden md:flex flex-none w-full md:w-1/4 px-4 mb-8 md:mb-0 items-start",children:[" ",(0,m.jsxs)("div",{className:"bg-white shadow-lg rounded-lg p-4 flex flex-col items-center self-start",children:[m.jsx("img",{src:a.images[0],alt:a.name,className:"h-48 object-cover rounded",style:{width:"100%",height:"200px",objectFit:"contain"}}),m.jsx("h4",{className:"font-semibold text-center",children:a.name}),m.jsx("p",{className:"text-gray-600 text-center",children:a.shortDescription}),m.jsx("a",{href:a.affiliateLink,target:"_blank",rel:"noopener noreferrer",className:"mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300",children:"View Product"})]})]}),(0,m.jsxs)("div",{className:"flex-1 max-w-2xl mb-8 md:mb-0",children:[" ",(0,m.jsxs)("div",{className:"bg-white shadow-lg rounded-lg p-6",children:[m.jsx("h2",{className:"text-3xl font-bold text-gray-800 mb-4 text-center",children:t.title}),(0,m.jsxs)("div",{className:"flex flex-col md:flex-row justify-between text-gray-600 mb-4",children:[(0,m.jsxs)("span",{children:[m.jsx("strong",{children:"Company:"})," ",t.company]}),(0,m.jsxs)("span",{children:[m.jsx("strong",{children:"Location:"})," ",t.location]})]}),(0,m.jsxs)("div",{className:"flex flex-col md:flex-row justify-between text-gray-600 mb-4",children:[(0,m.jsxs)("span",{children:[m.jsx("strong",{children:"Salary:"})," ",t.salary]}),(0,m.jsxs)("span",{children:[m.jsx("strong",{children:"Level:"})," ",t.level]})]}),(0,m.jsxs)("p",{className:"text-gray-600 mb-4",children:[m.jsx("strong",{children:"Description:"})," ",t.description]}),m.jsx("h3",{className:"text-xl font-bold mb-2",children:"Requirements:"}),m.jsx("ul",{className:"list-disc list-inside mb-4",children:t.requirements.map((e,i)=>m.jsx("li",{className:"text-gray-600",children:e},i))}),m.jsx("h3",{className:"text-xl font-bold mb-2",children:"Responsibilities:"}),m.jsx("ul",{className:"list-disc list-inside mb-4",children:t.responsibilities.map((e,i)=>m.jsx("li",{className:"text-gray-600",children:e},i))}),t.additionalInfo&&(0,m.jsxs)(m.Fragment,{children:[m.jsx("h3",{className:"text-xl font-bold mb-2",children:"Additional Information:"}),m.jsx("p",{className:"text-gray-600 mb-4",children:t.additionalInfo})]}),t.benefits&&(0,m.jsxs)(m.Fragment,{children:[m.jsx("h3",{className:"text-xl font-bold mb-2",children:"Benefits:"}),m.jsx("ul",{className:"list-disc list-inside mb-4",children:t.benefits.map((e,i)=>m.jsx("li",{className:"text-gray-600",children:e},i))})]}),t.applicationDeadline&&(0,m.jsxs)("div",{className:"text-gray-600 mb-4",children:[m.jsx("strong",{children:"Application Deadline:"})," ",t.applicationDeadline]}),t.workHours&&(0,m.jsxs)("div",{className:"text-gray-600 mb-4",children:[m.jsx("strong",{children:"Work Hours:"})," ",t.workHours]}),t.jobType&&(0,m.jsxs)("div",{className:"text-gray-600 mb-4",children:[m.jsx("strong",{children:"Job Type:"})," ",t.jobType]}),void 0!==t.remoteOptions&&(0,m.jsxs)("div",{className:"text-gray-600 mb-4",children:[m.jsx("strong",{children:"Remote Options:"})," ",t.remoteOptions?"Yes":"No"]}),m.jsx("h3",{className:"text-xl font-bold mb-2",children:"Tags:"}),m.jsx("div",{className:"flex flex-wrap mb-4",children:t.tags.map((e,i)=>m.jsx("span",{className:"bg-gray-200 text-gray-700 px-2 py-1 rounded-full text-sm mr-2 mb-2",children:e},i))}),(0,m.jsxs)("div",{className:"flex justify-center mt-6 space-x-4",children:[m.jsx("button",{onClick:()=>{alert(`Applying for ${t.title}`)},className:"bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition duration-200",children:"Apply Now"}),m.jsx("button",{onClick:()=>i(-1),className:"bg-gray-300 text-gray-800 px-6 py-2 rounded hover:bg-gray-400 transition duration-200",children:"Go Back"})]})]})]}),(0,m.jsxs)("div",{className:"flex-none w-full md:w-1/4 px-4 mb-8 md:mb-0",children:[" ",(0,m.jsxs)("div",{className:"bg-white shadow-lg rounded-lg p-4 flex flex-col items-center",children:[m.jsx("img",{src:s.images[0],alt:s.name,className:"h-48 object-cover rounded",style:{width:"100%",height:"200px",objectFit:"contain"}}),m.jsx("h4",{className:"font-semibold text-center",children:s.name}),m.jsx("p",{className:"text-gray-600 text-center",children:s.shortDescription}),m.jsx("a",{href:s.affiliateLink,target:"_blank",rel:"noopener noreferrer",className:"mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300",children:"View Product"})]})]})]})]})},x=(0,n.l)(a,"default"),b=(0,n.l)(a,"getStaticProps"),j=(0,n.l)(a,"getStaticPaths"),y=(0,n.l)(a,"getServerSideProps"),w=(0,n.l)(a,"config"),v=(0,n.l)(a,"reportWebVitals"),k=(0,n.l)(a,"unstable_getStaticProps"),N=(0,n.l)(a,"unstable_getStaticPaths"),S=(0,n.l)(a,"unstable_getStaticParams"),P=(0,n.l)(a,"unstable_getServerProps"),D=(0,n.l)(a,"unstable_getServerSideProps"),C=new s.PagesRouteModule({definition:{kind:r.x.PAGES,page:"/JobDetail",pathname:"/JobDetail",bundlePath:"",filename:""},components:{App:d(),Document:l()},userland:a})},2785:e=>{e.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},6689:e=>{e.exports=require("react")},4661:e=>{e.exports=require("react-router-dom")},997:e=>{e.exports=require("react/jsx-runtime")},5315:e=>{e.exports=require("path")}};var i=require("../webpack-runtime.js");i.C(e);var t=e=>i(i.s=e),a=i.X(0,[130,75],()=>t(5160));module.exports=a})();
---
+++ .next/static/chunks/pages/JobDetail-c5426bb149f609bf.js
@@ -0,0 +1,66 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[295],{4521:function(e,i,a){(window.__NEXT_P=window.__NEXT_P||[]).push(["/JobDetail",function(){return a(2952)}])},2952:function(e,i,a){"use strict";a.r(i),a.d(i,{default:function(){return l}});var t=a(5893);a(7294);var s=a(9250),r=a(9816);let n=[{id:1,name:"INIU Mini Powerbank",shortDescription:`
+              Compact and powerful, the INIU Mini Power Bank features a 10,000mAh capacity with 22.5W fast charging and USB-C compatibility. 
+              Charges up to 3 devices at once.
+          `,longDescription:`
+              The INIU Mini Power Bank offers a 10,000mAh capacity with 22.5W fast charging and USB-C in & out.
+        
+              **Key Features:**
+              - **Compatibility**: Works with iPhone 15/14/13/12/11, iPad, Samsung S22/S21, Xiaomi, Google devices, and more.
+              - **Performance**: Rated 4.6 out of 5 stars from 15,982 reviews.
+              - **Capacity**: Compact design, half the size of an iPhone, charges up to 3 devices simultaneously.
+              - **Support**: Includes a USB-C cable, travel case, user manual, and comes with a 30-day money-back guarantee, a 5-year warranty, and lifetime technical support.
+        
+              Priced at 242.65 kr with a 16% discount off the median price of 289.99 kr.
+          `,images:["/images/iniuminipowerbank.jpg","/images/iniuminipowerbank.jpg"],affiliateLink:"https://amzn.to/3BfMraU",price:"242,65 kr"},{id:2,name:"Xiaomi Smart Band 8",shortDescription:`
+              Sleek and versatile, the Xiaomi Smart Band 8 features a 1.62” AMOLED display, 16-day battery life, and 200+ customizable watch faces. 
+              Tracks health and supports 150+ workout modes.
+          `,longDescription:`
+              The Xiaomi Smart Band 8 features a sleek design with a metallic-like frame and quick-release strap for easy customization.
+        
+              **Key Features:**
+              - **Display**: Ambient light sensor with 60Hz refresh rate for smooth performance.
+              - **Battery Life**: Up to 16 days with 2C fast charging, fully recharged in about 1 hour.
+              - **Customization**: Choose from over 200 watch faces and track health with continuous SpO2 measurement, heart rate monitoring, and sleep analysis.
+              - **Exercise Tracking**: 150+ workout modes, supports unique accessories for versatile wear.
+        
+              Specifications include a 1.62” AMOLED screen, 5 ATM water resistance, and compatibility with Android 6.0 and iOS 12 or higher. Priced at 391,99 kr.
+          `,images:["/images/xioamismartband8.jpg","/images/xioamismartband8.jpg"],affiliateLink:"https://amzn.to/3XlditG",price:"391,99 kr"},{id:3,name:"Logitech C270 HD",shortDescription:`
+              The Logitech C270 HD Webcam offers 720p widescreen video, automatic light correction, and a noise-canceling microphone for clear calls.
+          `,longDescription:`
+              The Logitech C270 HD Webcam is designed for high-quality video calls with a 720p resolution and automatic light adjustment for better visibility in low-light conditions.
+        
+              **Key Features:**
+              - **Resolution**: HD 720p at 30fps for clear, widescreen video calls.
+              - **Microphone**: Built-in noise-canceling microphone for clear audio in noisy environments.
+              - **Light Adjustment**: Automatic light correction for better image quality in various lighting conditions.
+              - **Design**: Compact and adjustable, fits easily on monitors or stands on a shelf.
+              - **Compatibility**: Works with Skype, FaceTime, Hangouts, WebEx, and is certified for Chromebook.
+        
+              Priced at 190,24 kr with a 10% discount off the median price of 212,49 kr.
+          `,images:["/images/logitechc270.jpg","/images/logitechc270.jpg"],affiliateLink:"https://amzn.to/4e0DQrc",price:"190,24 kr"},{id:4,name:"Hand Grip Trainer",shortDescription:`
+              Enhance hand, finger, wrist, and forearm strength with the Vicloon Hand Grip Trainer. Adjustable resistance from 10-50 kg and non-slip handles for comfortable use.
+          `,longDescription:`
+              The Vicloon Hand Grip Trainer is perfect for improving grip strength and rehabilitating hand injuries.
+        
+              **Key Features:**
+              - **Adjustable Resistance**: Easily adjustable from 10-50 kg (22-110 lbs) using a rotating knob.
+              - **Non-Slip Handles**: Covered with eco-friendly TPE material for a secure grip, even when sweaty.
+              - **Portable**: Lightweight (150 g), compact (15.5 cm long), easy to carry and store.
+              - **Versatile**: Suitable for all ages and fitness levels, ideal for strengthening and rehabilitation.
+              - **Gift Idea**: Perfect for various occasions like birthdays and holidays.
+        
+              Priced at 73,30 kr with a 5% discount off the previous price of 77,16 kr, median price 89,99 kr.
+          `,images:["/images/vicloonhandgrip.jpg","/images/vicloonhandgrip.jpg"],affiliateLink:"https://amzn.to/3MOa0u3",price:"73,30 kr"},{id:5,name:"Philips 3000 Mixer",shortDescription:`
+            The Philips 3000 Series Mixer features a ProBlend system, 1.9 L max capacity, and a 450W motor. Includes a HomeID app with recipe ideas and a pulse function.
+        `,longDescription:`
+            The Philips 3000 Series Mixer offers efficient blending with a 1.9 L max capacity and a 450W motor. Perfect for smoothies and sauces.
+      
+            **Key Features:**
+            - **ProBlend System**: Designed for smooth blending with optimized blade and jar design.
+            - **Capacity**: 1.9 L maximum capacity, with a 1 L effective capacity.
+            - **Motor**: 450W motor for powerful performance and ice crushing.
+            - **Speed Settings**: One speed setting plus a pulse function for versatile use.
+            - **HomeID App**: Includes a free app with a variety of recipe ideas.
+      
+            Priced at 428,30 kr.
+        `,images:["/images/philips3000mixer.jpg","/images/philips3000mixer2.jpg"],affiliateLink:"https://amzn.to/3ziKeep",price:"428,30 kr"}],o=()=>{let e=Math.floor(Math.random()*n.length),i=Math.floor(Math.random()*n.length);for(;i===e;)i=Math.floor(Math.random()*n.length);return[n[e],n[i]]};var l=()=>{let{jobId:e}=(0,s.UO)(),i=(0,s.s0)(),a=r.Z.find(i=>i.id===e);if(!a)return(0,t.jsx)("div",{className:"text-center",children:"Job not found."});let[n,l]=o();return(0,t.jsxs)("div",{className:"relative bg-gradient-to-r from-purple-800 to-indigo-600 py-10",children:[" ",(0,t.jsx)("div",{className:"absolute inset-0 bg-black opacity-50"})," ",(0,t.jsxs)("div",{className:"container mx-auto flex flex-col md:flex-row justify-between px-4 relative z-10",children:[" ",(0,t.jsxs)("div",{className:"hidden md:flex flex-none w-full md:w-1/4 px-4 mb-8 md:mb-0 items-start",children:[" ",(0,t.jsxs)("div",{className:"bg-white shadow-lg rounded-lg p-4 flex flex-col items-center self-start",children:[(0,t.jsx)("img",{src:n.images[0],alt:n.name,className:"h-48 object-cover rounded",style:{width:"100%",height:"200px",objectFit:"contain"}}),(0,t.jsx)("h4",{className:"font-semibold text-center",children:n.name}),(0,t.jsx)("p",{className:"text-gray-600 text-center",children:n.shortDescription}),(0,t.jsx)("a",{href:n.affiliateLink,target:"_blank",rel:"noopener noreferrer",className:"mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300",children:"View Product"})]})]}),(0,t.jsxs)("div",{className:"flex-1 max-w-2xl mb-8 md:mb-0",children:[" ",(0,t.jsxs)("div",{className:"bg-white shadow-lg rounded-lg p-6",children:[(0,t.jsx)("h2",{className:"text-3xl font-bold text-gray-800 mb-4 text-center",children:a.title}),(0,t.jsxs)("div",{className:"flex flex-col md:flex-row justify-between text-gray-600 mb-4",children:[(0,t.jsxs)("span",{children:[(0,t.jsx)("strong",{children:"Company:"})," ",a.company]}),(0,t.jsxs)("span",{children:[(0,t.jsx)("strong",{children:"Location:"})," ",a.location]})]}),(0,t.jsxs)("div",{className:"flex flex-col md:flex-row justify-between text-gray-600 mb-4",children:[(0,t.jsxs)("span",{children:[(0,t.jsx)("strong",{children:"Salary:"})," ",a.salary]}),(0,t.jsxs)("span",{children:[(0,t.jsx)("strong",{children:"Level:"})," ",a.level]})]}),(0,t.jsxs)("p",{className:"text-gray-600 mb-4",children:[(0,t.jsx)("strong",{children:"Description:"})," ",a.description]}),(0,t.jsx)("h3",{className:"text-xl font-bold mb-2",children:"Requirements:"}),(0,t.jsx)("ul",{className:"list-disc list-inside mb-4",children:a.requirements.map((e,i)=>(0,t.jsx)("li",{className:"text-gray-600",children:e},i))}),(0,t.jsx)("h3",{className:"text-xl font-bold mb-2",children:"Responsibilities:"}),(0,t.jsx)("ul",{className:"list-disc list-inside mb-4",children:a.responsibilities.map((e,i)=>(0,t.jsx)("li",{className:"text-gray-600",children:e},i))}),a.additionalInfo&&(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)("h3",{className:"text-xl font-bold mb-2",children:"Additional Information:"}),(0,t.jsx)("p",{className:"text-gray-600 mb-4",children:a.additionalInfo})]}),a.benefits&&(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)("h3",{className:"text-xl font-bold mb-2",children:"Benefits:"}),(0,t.jsx)("ul",{className:"list-disc list-inside mb-4",children:a.benefits.map((e,i)=>(0,t.jsx)("li",{className:"text-gray-600",children:e},i))})]}),a.applicationDeadline&&(0,t.jsxs)("div",{className:"text-gray-600 mb-4",children:[(0,t.jsx)("strong",{children:"Application Deadline:"})," ",a.applicationDeadline]}),a.workHours&&(0,t.jsxs)("div",{className:"text-gray-600 mb-4",children:[(0,t.jsx)("strong",{children:"Work Hours:"})," ",a.workHours]}),a.jobType&&(0,t.jsxs)("div",{className:"text-gray-600 mb-4",children:[(0,t.jsx)("strong",{children:"Job Type:"})," ",a.jobType]}),void 0!==a.remoteOptions&&(0,t.jsxs)("div",{className:"text-gray-600 mb-4",children:[(0,t.jsx)("strong",{children:"Remote Options:"})," ",a.remoteOptions?"Yes":"No"]}),(0,t.jsx)("h3",{className:"text-xl font-bold mb-2",children:"Tags:"}),(0,t.jsx)("div",{className:"flex flex-wrap mb-4",children:a.tags.map((e,i)=>(0,t.jsx)("span",{className:"bg-gray-200 text-gray-700 px-2 py-1 rounded-full text-sm mr-2 mb-2",children:e},i))}),(0,t.jsxs)("div",{className:"flex justify-center mt-6 space-x-4",children:[(0,t.jsx)("button",{onClick:()=>{alert(`Applying for ${a.title}`)},className:"bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition duration-200",children:"Apply Now"}),(0,t.jsx)("button",{onClick:()=>i(-1),className:"bg-gray-300 text-gray-800 px-6 py-2 rounded hover:bg-gray-400 transition duration-200",children:"Go Back"})]})]})]}),(0,t.jsxs)("div",{className:"flex-none w-full md:w-1/4 px-4 mb-8 md:mb-0",children:[" ",(0,t.jsxs)("div",{className:"bg-white shadow-lg rounded-lg p-4 flex flex-col items-center",children:[(0,t.jsx)("img",{src:l.images[0],alt:l.name,className:"h-48 object-cover rounded",style:{width:"100%",height:"200px",objectFit:"contain"}}),(0,t.jsx)("h4",{className:"font-semibold text-center",children:l.name}),(0,t.jsx)("p",{className:"text-gray-600 text-center",children:l.shortDescription}),(0,t.jsx)("a",{href:l.affiliateLink,target:"_blank",rel:"noopener noreferrer",className:"mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300",children:"View Product"})]})]})]})]})}}},function(e){e.O(0,[774,644,250,816,888,179],function(){return e(e.s=4521)}),_N_E=e.O()}]);
